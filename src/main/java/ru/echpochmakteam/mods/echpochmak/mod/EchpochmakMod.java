d5bd008ec7095c6bd4370a2d1cb96173fbee55ab
Doctor Night <72402692+DoctorNight799@users.noreply.github.com>
2020-10-05 20:00:29 +0300

    Update EchpochmakMod.java

--- src/main/java/ru/echpochmakteam/mods/echpochmak/mod/EchpochmakMod.java
+++ src/main/java/ru/echpochmakteam/mods/echpochmak/mod/EchpochmakMod.java
@@ -11,8 +11,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class EchpochmakMod implements ModInitializer {
-	public static final Item ECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(6f).hunger(4).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION,20*10),1f).build()));
-	public static final Item SUPERECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(12f).hunger(8).build()));
+	public static final Item ECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(10f).hunger(14).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE,20*5),0.5f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*5),0.5f).build()));
+	public static final Item SUPERECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(20f).hunger(20).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20*60), 1f).statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 20*180),1f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION,20*60),1f).build()));
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("echpochmakmod", "echpochmak"), ECHPOCHMAK);
