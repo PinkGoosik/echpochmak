af31e9640f464d254c56048435d172062d85fa70
Doctor Night <72402692+DoctorNight799@users.noreply.github.com>
2020-10-05 21:17:11 +0300

    Update EchpochmakMod.java

--- src/main/java/ru/echpochmakteam/mods/echpochmak/mod/EchpochmakMod.java
+++ src/main/java/ru/echpochmakteam/mods/echpochmak/mod/EchpochmakMod.java
@@ -13,6 +13,7 @@ import net.minecraft.util.registry.Registry;
 public class EchpochmakMod implements ModInitializer {
 	public static final Item ECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(10f).hunger(14).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE,20*5),0.5f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*5),0.5f).build()));
 	public static final Item SUPERECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(20f).hunger(20).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20*60), 1f).statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 20*180, 4),1f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION,20*60, 3),1f).build()));
+	public static final Item ONION = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(2f).hunger(1).statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20*10),1f).build()));
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("echpochmakmod", "echpochmak"), ECHPOCHMAK);
